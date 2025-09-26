1c56c73a26af94cab5cea08b35917b084e124a47
fhawas87 <jakubwozniak775@gmail.com>
2025-09-26 21:31:12 +0200

    Created sampler class which is filling structure of cpu/gpu/ram with real time data. Later it will be called every x s/ms and will be refreshed in the dashboard.

--- src/app_render/components_dashboard/sample.h
+++ src/app_render/components_dashboard/sample.h
@@ -2,8 +2,8 @@
 
 #include <vector>
 #include <string>
-#include <chrono>
-#include <thread>
+//#include <chrono>
+//#include <thread>
 
 #include "cpu.h"
 #include "gpu.h"
@@ -11,38 +11,6 @@
 
 #include <imgui.h>
 
-/*
-struct dashboard_stats {
-  
-  unsigned int ram_memory = get_ram_memory_usage();
-  unsigned int gpu_temp = get_current_gpu_temperature();
-  unsigned int gpu_usage = get_core_utilization_percentage_rate();
-  unsigned int gpu_freq = get_gpu_clock_frequency();
-
-  std::string gpu_model = get_accessible_device_name();
-  std::string cpu_model = get_cpu_model();
-  
-  std::vector<unsigned int> gpu_vram = get_gpu_VRAM_info();
-  std::vector<float> cpu_temps = get_cpu_core_thermal_values();
-  std::vector<float> cpu_freqs = get_cpu_core_frequencies(cpu_temps.size());
-
-  float cpu_usage = get_cpu_utilization();
-
-  unsigned int ram_memory;
-  unsigned int gpu_temp;
-  unsigned int gpu_usage;
-  unsigned int gpu_freq;
-
-  std::string gpu_model;
-  std::string cpu_model;
-
-  std::vector<unsigned int> gpu_vram;
-  std::vector<float> cpu_temps;
-  std::vector<float> cpu_freqs;
-
-  float cpu_usage;
-};
-*/
 
 struct cpu_stats {
 
@@ -84,7 +52,10 @@ struct stats {
 class sampler {
 public :
   
-  stats sample;
+  // returning empty sample structure is only for zero/empty initialization
+  // I could do it in every structure above the class but this looks cleaner
+
+  const stats &snaphot() const { return sample; } 
 
   void refresh_samples() {
 
@@ -98,7 +69,13 @@ public :
     sample.gpu.gpu_freq  = get_gpu_clock_frequency();
     sample.gpu.gpu_vram  = get_gpu_VRAM_info();
     sample.gpu.gpu_usage = get_core_utilization_percentage_rate();
+
+    sample.ram.ram.usage = get_ram_memory_usage();
   }
+
+private:
+
+  stats sample{};
 };
 
 
