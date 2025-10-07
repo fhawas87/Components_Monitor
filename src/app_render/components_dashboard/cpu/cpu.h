fdf8f0b8aeee73e65022fe9d241de2cd9810b9bb
fhawas87 <jakubwozniak775@gmail.com>
2025-10-07 15:21:43 +0200

    Getting cpu model name here now to make it global

--- src/app_render/components_dashboard/cpu/cpu.h
+++ src/app_render/components_dashboard/cpu/cpu.h
@@ -4,34 +4,8 @@
 #include <vector>
 #include <cstdlib>
 
-/*class cpu {
-
-public :
-
-  cpu_model();
-
-  void load_sample_data() {
-
-    get_cpu_core_thermal_values();
-    get_cpu_core_frequencies();
-    get_cpu_utilization();
-  }
-
-private :
-  
-  std::string cpu_model();
-  
-  std::vector<float> get_cpu_core_thermal_values();
-  std::vector<float> get_cpu_core_frequencies(const size_t& number_of_cores);
-
-  float get_cpu_utilization();
-
-
-};
-*/
-
-
-std::string cpu_model = get_cpu_model();
+std::string get_cpu_model();
+inline std::string cpu_model = get_cpu_model();
 
 std::vector<float> get_cpu_core_thermal_values();
 std::vector<float> get_cpu_core_frequencies();
