7763cdf74fccdd902e7c441fa819f182edfd3b6b
fhawas87 <jakubwozniak775@gmail.com>
2025-10-07 02:28:05 +0200

    Implemented function which retrieves stock cpu frequencie for setting y axis limit for cpu per core frequencie chart

--- src/app_render/components_dashboard/cpu/cpu.h
+++ src/app_render/components_dashboard/cpu/cpu.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 /*class cpu {
 
@@ -30,10 +31,29 @@ private :
 */
 
 
-std::string get_cpu_model();
+std::string cpu_model = get_cpu_model();
 
 std::vector<float> get_cpu_core_thermal_values();
 std::vector<float> get_cpu_core_frequencies();
-//std::vector<float> get_cpu_core_frequencies(size_t number_of_cores);
 
 float get_cpu_utilization();
+
+static unsigned int retrieve_fabric_cpu_freq(std::string &cpu_model_name) {
+  
+  const char *char_pointer = cpu_model_name.c_str();
+  char cpu_freq_buffer[4];
+  cpu_freq_buffer[3] = '\0';
+  
+  while (*char_pointer != '@') { char_pointer++; }
+  
+  if (*char_pointer == '@') {
+
+    char_pointer++;
+    char_pointer++;
+    cpu_freq_buffer[0] = *char_pointer; char_pointer++; char_pointer++;
+    cpu_freq_buffer[1] = *char_pointer; char_pointer++;
+    cpu_freq_buffer[2] = *char_pointer;
+  }
+
+  return (10 * std::atoi(cpu_freq_buffer));
+}
