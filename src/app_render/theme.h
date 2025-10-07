193ab4efdfd83199ba54d49adac0940fedfa36af
fhawas87 <jakubwozniak775@gmail.com>
2025-10-07 15:27:40 +0200

    Added light grey Vec. Also few smaller changes

--- src/app_render/theme.h
+++ src/app_render/theme.h
@@ -3,11 +3,13 @@
 #include <imgui.h>
 
 struct Theme {
+  
+  const ImVec4 LightGrey = ImVec4(0.96f, 0.96f, 0.96f, 1.0f);
 
   ImVec4 window_bg = ImVec4(0.16f, 0.16f, 0.16f, 1.0f);
 
-  float window_rounding = 10.0f;
-  float frame_rounding = 5.0f;
+  //float window_rounding = 10.0f;
+  //float frame_rounding = 5.0f;
   float padding = 8.0f;
 
   void ApplyTheme() {
@@ -15,8 +17,8 @@ struct Theme {
     ImGuiStyle& s = ImGui::GetStyle();
 
     s.Colors[ImGuiCol_WindowBg] = window_bg;
-    s.WindowRounding = window_rounding;
-    s.FrameRounding = frame_rounding;
+    //s.WindowRounding = window_rounding;
+    //s.FrameRounding = frame_rounding;
     s.WindowPadding = ImVec2(padding, padding);
   }
 };
