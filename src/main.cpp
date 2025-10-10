655d11e93e47c652c75265943fa4f1bb4e8692d0
fhawas87 <jakubwozniak775@gmail.com>
2025-10-10 11:33:33 +0200

    Modified code structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
-#include <stdio.h>
-
-//#include <sensors/sensors.h>
-//#include <nvml.h>
-
 #include <render.h>
 
 int main(void) {
   
-  // RUN APP
-  
-  /*
-  nvmlReturn_t initialize_result;
-  initialize_result = nvmlInit_v2();
-  if (initialize_result != NVML_SUCCESS) {
-    printf("\nERROR CODE : ( %d ), info : '%s'\n", (int)initialize_result, nvmlErrorString(initialize_result));
-    nvmlShutdown();
-
-    return 1;
-  }
-  if (sensors_init(NULL) != 0) {
-    printf("\nERROR code : libsensors library initialization error\n");
-    sensors_cleanup();
-
-    return 1;
-  }
-  */
-
   MainWindow app;
 
   return app.run_window();
